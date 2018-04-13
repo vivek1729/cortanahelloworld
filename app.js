03e1a71e24a2e517a0a27a6b9d5e61aff38c07a3
vivek1729 <sasuke.vivek@gmail.com>
2018-04-12 18:55:43 -0700

    Added ishani changes

--- app.js
+++ app.js
@@ -51,6 +51,7 @@ var tableStorage = new botbuilder_azure.AzureBotStorage({ gzipData: false }, azu
 var store = require('data-store')('reminders');
 //store.del('a');
 store.set('user_map',{'1234':'abhay','127863':'ishani'});
+/*
 var bot = new builder.UniversalBot(connector, [
     function (session) {
         session.send("Welcome to family reminder");
@@ -103,10 +104,63 @@ var bot = new builder.UniversalBot(connector, [
         session.endDialog();
     }
 ]);
-/*
-var bot = new builder.UniversalBot(connector);
-//bot.set('storage', tableStorage);
+*/
 
-bot.dialog('/', function (session) {
-    session.send('You said ' + session.message.text);
-});*/
+var bot = new builder.UniversalBot(connector);
+bot.dialog('/',[
+//You use the session.send method to send messages in response to a message from the user.
+    function (session,args,next){
+        var userId = session.message.user.id;
+        var user_map = store.get('user_map');
+        if(!user_map.hasOwnProperty(userId)){
+           builder.Prompts.text(session, "Hi, What is your cool nickname ?");
+        }else{
+          next();
+        }
+       // builder.Prompts.text(session, "Hi, Who do you want to send the reminder ?");
+        //session.beginDialog('Hi, Who do you want to send the reminder ?');
+        //builder.Prompts.text(session,"Hi, Who do you want to send the reminder ?");
+    },
+    function(session,results){
+      if(results.response){
+          var userId= session.message.user.id; 
+          var user_map = store.get('user_map');
+          user_map[userId] = results.response;
+          store.set('user_map',user_map);
+          builder.Prompts.text(session, "Cool, Who do you want to send the reminder ?");
+      }else{
+          builder.Prompts.text(session, "Hi, Who do you want to send the reminder ?");
+      }
+    },
+    function (session, results){
+        session.dialogData.receiver= results.response;
+        builder.Prompts.text(session, "What is the reminder you want to send ?");
+        //session.beginDialog('What is the reminder you want to send ?');
+    },
+    function (session, results) {
+        session.dialogData.task = results.response;
+        session.send('Reminder Sent !');
+        var reminder_obj = {'task':session.dialogData.task,'receiver':session.dialogData.receiver};
+        var user_map = store.get('user_map');
+        var user_id = null;
+        for (var key in user_map) {
+            if (user_map.hasOwnProperty(key) && user_map[key] === session.dialogData.receiver) {
+                user_id = key;
+                break;
+            }
+        }
+        if(user_id != null){
+            var user_reminders = []
+            if(store.has(user_id)){
+                user_reminders = store.get(user_id);
+            }
+            reminder_obj.receiver = user_id;
+            reminder_obj.created_by = session.message.user.id;
+            user_reminders.push(reminder_obj);
+            store.set(user_id,user_reminders);
+            //Set reminders, yay!
+        }
+        console.log(store.get());//Log everything in store
+    },
+
+]);
